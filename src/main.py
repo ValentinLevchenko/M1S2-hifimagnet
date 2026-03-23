535eafb8ac75c304e09b3412b919c5c4cea778a4
Valentin Levchenko <valentinlevchenko@Host-001.lan>
2026-03-23 23:26:04 +0100

    Initialise the project structure (Closes #2, #3; Addresses #4)
    created the skeleton structure
    
    added minimal .gitignore
    
    added README.md (yet empty)

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("\nProjet HIFI-magnet (Version 0.0.1);\n")
+
+print("Author: Valentin Levchenko, Student ID: 22422485 M1S2")
+print("Program: M1S2, CSMI, University of Strasbourg\n")
+
+print("Supervisors:")
+print("Dr. Christophe Trophime, Laboratoire Nationale des Champs Magnétiques Intenses, LNCMI")
+print("Prof. Christophe Prud'homme, University of Strasbourg")
+print("Asst. Prof. Joubine Aghili, University of Strasbourg\n")
